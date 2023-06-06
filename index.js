ae4557b07121d76bc614cac791dfdd7d46b94669
michael <michael@telemessage.com>
2023-06-06 13:04:16 +0300

    changed outbound rate limit code

--- index.js
+++ index.js
@@ -652,7 +652,7 @@ exports.outbound_increment = async function (next, hmail) {
         } else {
 
             let requestedDelayInSeconds = 1 / rate;
-            let currentDelayInSeconds = currentMessageTime.getSeconds() - new Date(Date.parse(lastSentMessageForDomain)).getSeconds();
+            let currentDelayInSeconds = Math.floor((currentMessageTime.getTime() - lastSentMessageForDomain.getTime()) / 1000);
 
             this.loginfo("rate limit plugin: for domain " + outDom + " requestedDelayInSeconds " + requestedDelayInSeconds +
                 " currentDelayInSeconds " + currentDelayInSeconds + " currentMessageTime " + currentMessageTime);
